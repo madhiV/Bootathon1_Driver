b1654a6d9477912253f40c5d30dada2418de6bdd
madhiV <vadhananmadhi@gmail.com>
2021-08-18 07:47:34 +0530

    Modified tabServer constructor

--- src/driver/TabServer.java
+++ src/driver/TabServer.java
@@ -3,7 +3,13 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.Event;
 public class TabServer extends JTabbedPane{
+    JPanel login,signup,acceptRide,ProvideFeedback;
+    TextField txtFld1,txtFld2;
+    Label lbl1,lbl2,lbl3;
     TabServer(){
-
+        setBounds(10,10,1000,2000);
+        setVisible(true);
+        setLayout(new BorderLayout());
+        setFont(new Font("Times new roman",Font.BOLD,18));
     }
 }
