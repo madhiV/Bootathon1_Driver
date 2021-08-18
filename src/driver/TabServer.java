e62afb30571816f91d441669ac8c69e04535c046
madhiV <vadhananmadhi@gmail.com>
2021-08-18 09:42:06 +0530

    extended JPanel for all classes and added import statements

--- src/driver/TabServer.java
+++ src/driver/TabServer.java
@@ -2,11 +2,13 @@ package driver;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.Event;
-public class TabServer extends JTabbedPane{
+public class TabServer extends JFrame{
+    JTabbedPane tabs;
     JPanel login,signup,acceptRide,ProvideFeedback;
     TextField txtFld1,txtFld2;
     Label lbl1,lbl2,lbl3;
     TabServer(){
+        tabs=new JTabbedPane();
         setBounds(10,10,1000,2000);
         setVisible(true);
         setLayout(new BorderLayout());
