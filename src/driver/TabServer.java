0679f23f341b426e5e077d23025e7286e2b06a5c
madhiV <vadhananmadhi@gmail.com>
2021-08-18 09:51:23 +0530

    Made mods to TabServer constructor

--- src/driver/TabServer.java
+++ src/driver/TabServer.java
@@ -4,14 +4,27 @@ import java.awt.*;
 import java.awt.Event;
 public class TabServer extends JFrame{
     JTabbedPane tabs;
-    JPanel login,signup,acceptRide,ProvideFeedback;
+    JPanel login,signup,acceptRide,feedback,update;
     TextField txtFld1,txtFld2;
     Label lbl1,lbl2,lbl3;
-    TabServer(){
+    public TabServer(){
+        setTitle("DRIVER'S HOME");
         tabs=new JTabbedPane();
         setBounds(10,10,1000,2000);
         setVisible(true);
-        setLayout(new BorderLayout());
         setFont(new Font("Times new roman",Font.BOLD,18));
+        add(tabs,BorderLayout.CENTER);
+        login=new Login();
+        signup=new SignUp();
+        acceptRide=new AcceptRide();
+        feedback =new ProvideFeedback();
+        update=new UpdateProfile();
+        tabs.addTab("LOGIN",login);
+        tabs.addTab("SIGNUP",signup);
+        tabs.addTab("ACCEPT RIDES",acceptRide);
+        tabs.addTab("UPDATE PROFILE",update);
+        tabs.addTab("PROVIDE FEEDBACK",feedback);
+
+        add(tabs);
     }
 }
--- src/driver/UpdateProfile.java
+++ src/driver/UpdateProfile.java
@@ -1,4 +1,9 @@
 package driver;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.Event.*;
+public class UpdateProfile extends JPanel{
+    UpdateProfile(){
 
-public class UpdateProfile {
+    }
 }
